b2f70c1c6ca670883287b01ff3fbeafc96128aab
Izabela Furdzik <izabela.furdzik@pracuj.pl>
2021-04-11 22:31:29 +0200

    feat: change forms

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,12 +4,18 @@ import 'retinajs';
 const playVideo = () => {
   const videoPlayBtn = document.getElementById('video-play');
   const video = document.getElementById('video');
+  const videoBox = document.getElementById('video-box');
+  const form = document.getElementById('form');
 
   videoPlayBtn.addEventListener('click', () => {
     videoPlayBtn.style.display = 'none';
     video.play();
   });
-}
 
+  video.addEventListener('ended', () => {
+    form.style.display = 'block';
+    videoBox.style.display = 'none';
+  },false)
+}
 
 playVideo();
